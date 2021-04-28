a0ecaca6bf51dbf52a7217da6a80bd997b00bc31
Hector Saravia <hector.saravia@mail.udp.cl>
2021-04-28 13:52:02 -0400

    changed 'text/plain' to 'text/html'

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const http = require('http');
+const {createReadStream} = require('fs')
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  createReadStream('source/index.html').pipe(res);
 });
 
 server.listen(port, hostname, () => {
